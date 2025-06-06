9e927c9f6b3681773d3ab05aacfff7f31b7c42f3
JAMA <jose.munoz.alv@usach.cl>
2025-06-06 16:15:06 -0400

    Ajustes en conexiones con BDD de ms-user.

--- ms-6/src/main/java/com/tingeso/ms_6/Ms6Application.java
+++ ms-6/src/main/java/com/tingeso/ms_6/Ms6Application.java
@@ -2,8 +2,10 @@ package com.tingeso.ms_6;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients(basePackages = "com.tingeso.ms_6.clients")
 public class Ms6Application {
 
 	public static void main(String[] args) {
