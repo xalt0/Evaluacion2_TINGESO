e443bd724d4ee3935140b05ea7bbfc17a512d603
JAMA <jose.munoz.alv@usach.cl>
2025-06-06 23:17:44 -0400

    Test de gateway-server.

--- karting-frontend/src/http-common.js
+++ karting-frontend/src/http-common.js
@@ -4,7 +4,7 @@ const kartingBackendServer = import.meta.env.VITE_KARTING_BACKEND_SERVER;
 const kartingBackendPort = import.meta.env.VITE_KARTING_BACKEND_PORT;
 
 const httpClient = axios.create({
-  baseURL: `${kartingBackendServer}:${kartingBackendPort}`,
+  baseURL: 'http://localhost:8080', // Gateway, no microservicio directo
   headers: {
     "Content-Type": "application/json"
   }
