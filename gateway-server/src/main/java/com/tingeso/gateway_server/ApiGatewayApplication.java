051d5997455db87250f5a9b28e31202f407e064d
JAMA <jose.munoz.alv@usach.cl>
2025-06-09 23:30:44 -0400

    Ajustes necesarios para despliegue en minikube.

--- gateway-server/src/main/java/com/tingeso/gateway_server/ApiGatewayApplication.java
+++ gateway-server/src/main/java/com/tingeso/gateway_server/ApiGatewayApplication.java
@@ -2,12 +2,12 @@ package com.tingeso.gateway_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-public class GatewayServerApplication {
+public class ApiGatewayApplication {
+
 	public static void main(String[] args) {
-		SpringApplication.run(GatewayServerApplication.class, args);
+		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
+
 }
