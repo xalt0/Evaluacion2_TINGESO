05718f2b012ed9c370c019107f7dc29e0ea31256
JAMA <jose.munoz.alv@usach.cl>
2025-06-10 16:52:46 -0400

    Ajustes menores.

--- karting-frontend/src/http-common.js
+++ karting-frontend/src/http-common.js
@@ -4,7 +4,7 @@ const kartingBackendServer = import.meta.env.VITE_KARTING_BACKEND_SERVER;
 const kartingBackendPort = import.meta.env.VITE_KARTING_BACKEND_PORT;
 
 const httpClient = axios.create({
-  baseURL: 'http://localhost:8080', // Gateway, no microservicio directo
+  baseURL: `http://${kartingBackendServer}:${kartingBackendPort}`,
   headers: {
     "Content-Type": "application/json"
   }
